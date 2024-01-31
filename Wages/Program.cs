2254588a13ac2e14d17b8e4a6b9e3d31a341b244
Mathew Desjardins <desjardinsm@etsu.edu>
2024-01-31 12:37:32 -0500

    Add project files.

---
+++ Wages/Program.cs
@@ -0,0 +1,26 @@
+﻿//Variables
+
+string name;
+string hourlyPayAsString;
+double hourlyPay;
+double hourlyOvertimePay;
+
+//Program
+Console.WriteLine("Hello! Welcome to my program!");
+
+Console.Write("What is your name? ");
+
+name = Console.ReadLine();
+
+Console.Write("What is your hourly pay? ");
+
+//using Convert to change a string to a double
+//hourlyPay = Convert.ToDouble(Console.ReadLine());
+
+//using the data types Parse
+hourlyPay = double.Parse(Console.ReadLine());
+
+
+//my pseudo test area
+Console.WriteLine($"Hello! {name}");
+
